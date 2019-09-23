74fee08785198b9a82cfefa94db5d2ed28d376a5
anil reddy <anilroyal867@gmail.com>
2019-09-23 09:14:14 +0530

    add recipes and ingredients and directions

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,15 @@
 class Recipe < ApplicationRecord
+  belongs_to :user
+  has_many :ingredients
+  has_many :directions
+  mount_uploader :image, ImageUploader
+
+  accepts_nested_attributes_for :ingredients,
+                                reject_if: proc { |attributes| attributes['name'].blank? },
+                                allow_destroy: true
+  accepts_nested_attributes_for :directions,
+                                reject_if: proc { |attributes| attributes['step'].blank? },
+                                allow_destroy: true
+
+  validates :title, :description, :image, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_20_160446) do
+ActiveRecord::Schema.define(version: 2019_09_23_031718) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "directions", force: :cascade do |t|
+    t.text "step"
+    t.bigint "recipe_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["recipe_id"], name: "index_directions_on_recipe_id"
+  end
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+    t.bigint "recipe_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["recipe_id"], name: "index_ingredients_on_recipe_id"
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.string "title"
@@ -21,6 +37,21 @@ ActiveRecord::Schema.define(version: 2019_09_20_160446) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "directions", "recipes"
+  add_foreign_key "ingredients", "recipes"
 end
